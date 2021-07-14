af81863f81f813f8fe9b61f4bbdb9dcd5a98e914
Ledda Bryan <louchebem06@gmail.com>
2021-07-14 08:33:58 +0200

    for Mathieu

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 00:16:35 by mmehran           #+#    #+#             */
-/*   Updated: 2021/07/14 06:25:33 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/14 08:33:37 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,42 @@ void	print_array(char **arr)
 	}
 }
 
+void	clean_arg(char **arg)
+{
+	char	*tmp;
+	int		i;
+
+	tmp = ft_strtrim(*arg, " ");
+	free(*arg);
+	*arg = ft_strdup(tmp);
+	free(tmp);
+	tmp = 0;
+	if (*arg[0] == '"' && *arg[ft_strlen(*arg) - 1] == '"'
+		&& ft_count_char(*arg, '$') > 0)
+	{
+		i = 0;
+		while (*arg[i] != '$')
+			i++;
+		if (*arg[i] == '$')
+		{
+			i++;
+			while (ft_isspace(*arg[i]) == 0)
+			{
+				add_char(&tmp, *arg[i])
+				i++;
+			}
+			if (ft_strlen(tmp) > 1)
+				ft_replace_str(arg, tmp, ft_getenv(tmp));
+			free(tmp);
+		}
+	}
+}
+
 char	**parsing(char *str)
 {
 	char	**pars;
 	int		ac;
+	int		i;
 
 	if (!is_valid(str))
 	{
@@ -34,6 +66,9 @@ char	**parsing(char *str)
 	ac = count_args(str);
 	pars = ft_calloc(sizeof(char *), ac + 1);
 	split_args(pars, str);
+	i = -1;
+	while (pars[++i])
+		clean_arg(&pars[i]);
 	print_array(pars);
 	return (pars);
 }
