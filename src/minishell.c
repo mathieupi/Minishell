109c532d35a44eb1e0dbaf2f129bb75133181866
Ledda Bryan <louchebem06@gmail.com>
2021-07-17 18:54:27 +0200

    2021-07-17T18:54:27+02:00

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmehran <mmehran@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 14:24:07 by mmehran           #+#    #+#             */
-/*   Updated: 2021/07/16 11:55:44 by mmehran          ###   ########.fr       */
+/*   Updated: 2021/07/17 18:54:22 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	minishell(void)
 		if (in[0])
 			add_history(in);
 		argv = parsing(in);
+		update_env("_", argv[count_array(argv) - 1]);
 		try_exec(argv);
 		free(prompt);
 		free_array(argv);
