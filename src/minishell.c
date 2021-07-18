5c07eeab6c2a79d5be2565eac1d98de36bc90ab0
Bryan Ledda <louchebem06@gmail.com>
2021-07-18 03:27:37 +0200

    Sun Jul 18 03:27:37 CEST 2021

--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,42 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 14:24:07 by mmehran           #+#    #+#             */
-/*   Updated: 2021/07/18 02:49:37 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/18 03:26:52 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+static void	try_cmds(t_redirection	**redict)
+{
+	char	**argv;
+	int		j;
+
+	j = 0;
+	while (redict[j])
+	{
+		printf("Type : %d\n", redict[j]->type);
+		if (redict[j]->value)
+		{
+			argv = parsing(redict[j]->value);
+			if (count_array(argv) > 0)
+				update_env("_", argv[count_array(argv) - 1]);
+			try_exec(argv);
+			free_array(argv);
+			free(redict[j]->value);
+		}
+		j++;
+		if (redict[j] && redict[j]->value == NULL)
+			break ;
+	}
+	free_array((char **)redict);
+}
+
 void	minishell(void)
 {
 	char			*in;
-	char			**argv;
 	char			*prompt;
 	t_redirection	**redict;
-	int				j;
 
 	while (1)
 	{
@@ -30,24 +53,7 @@ void	minishell(void)
 			add_history(in);
 		redict = cmds(in);
 		free(in);
-		j = 0;
-		while (redict[j])
-		{
-			printf("Type : %d\n", redict[j]->type);
-			if (redict[j]->value)
-			{
-				argv = parsing(redict[j]->value);
-				if (count_array(argv) > 0)
-					update_env("_", argv[count_array(argv) - 1]);
-				try_exec(argv);
-				free_array(argv);
-				free(redict[j]->value);
-			}
-			j++;
-			if (redict[j] && redict[j]->value == NULL)
-				break ;
-		}
-		free_array((char **)redict);
+		try_cmds(redict);
 		free(prompt);
 	}
 }
