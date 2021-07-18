f1e972b631d2e5e0cd3443c243f92d8105a0fe8d
Ledda Bryan <louchebem06@gmail.com>
2021-07-18 14:03:45 +0200

    2021-07-18T14:03:45+02:00

--- src/redirection.c
+++ src/redirection.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 20:51:30 by bledda            #+#    #+#             */
-/*   Updated: 2021/07/18 03:13:35 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/18 14:03:26 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static int	isredirection(char *str)
 	return (i);
 }
 
-static int	size_args(char **str)
+static int	count_cmds(char **str)
 {
 	int	i;
 	int	j;
@@ -61,18 +61,19 @@ t_redirection	**cmds(char *str)
 	ac = count_args(str);
 	tmp = ft_calloc(sizeof(char *), ac + 1);
 	split_args(tmp, str);
-	redict = ft_calloc(sizeof(t_redirection), size_args(tmp) + 1);
+	redict = ft_calloc(sizeof(t_redirection), count_cmds(tmp) + 1);
 	i = -1;
 	j = 0;
 	redict[j] = ft_calloc(sizeof(t_redirection), 1);
 	while (tmp[++i])
+	{
 		if (isredirection(tmp[i]) == 0)
 			add_value(&redict[j]->value, tmp[i]);
-	else
-	{
-		j++;
-		redict[j] = ft_calloc(sizeof(t_redirection), 1);
-		redict[j]->type = isredirection(tmp[i]);
+		else
+		{
+			redict[++j] = ft_calloc(sizeof(t_redirection), 1);
+			redict[j]->type = isredirection(tmp[i]);
+		}
 	}
 	free_array(tmp);
 	return (redict);
