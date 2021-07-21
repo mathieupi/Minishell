f982e8c260c400d114f1290be057118df1862bd7
Ledda Bryan <louchebem06@gmail.com>
2021-07-21 17:41:09 +0200

    2021-07-21T17:41:08+02:00

--- src/parser/update_struct.c
+++ src/parser/update_struct.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 06:23:22 by bledda            #+#    #+#             */
-/*   Updated: 2021/07/21 17:38:00 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/21 17:41:04 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,13 @@ void	update_struct2(char c, t_parsing *parsing)
 		is_inhiber = true;
 
 	parsing->in_squote = is_simple2;
+	parsing->in_dquote = is_double2;
 
 	if (c == '\'' && !is_double)
 		is_simple = !is_simple;
-
-	is_simple2 = is_simple;
-
-	parsing->in_dquote = is_double2;
-
-	if (c == '"' && !is_simple)
+	else if (c == '"' && !is_simple)
 		is_double = !is_double;
-
+	
+	is_simple2 = is_simple;
 	is_double2 = is_double;
 }
