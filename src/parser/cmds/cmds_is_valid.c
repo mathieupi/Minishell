fa7822f60d361e1600c17356f04760df5b7de400
Ledda Bryan <louchebem06@gmail.com>
2021-07-23 02:10:46 +0200

    2021-07-23T02:10:45+02:00

--- src/parser/cmds/cmds_is_valid.c
+++ src/parser/cmds/cmds_is_valid.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 02:01:27 by bledda            #+#    #+#             */
-/*   Updated: 2021/07/23 02:01:35 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/23 02:10:14 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,10 @@ static char	*string_char(int nb)
 	return (tmp);
 }
 
-bool	cmds_is_valid(t_cmd	**cmds)
+static bool	first_is_command(t_cmd **cmds)
 {
-	int		i;
 	char	*str_error;
 
-	i = -1;
 	if (cmds[1] && cmds[0]->type == 0 && cmds[0]->str == 0
 		&& cmds[1]->type != 0)
 	{
@@ -51,28 +49,53 @@ bool	cmds_is_valid(t_cmd	**cmds)
 		free(str_error);
 		return (false);
 	}
+	return (true);
+}
+
+static bool	is_double_semicolon(t_cmd **cmds, int i)
+{
+	if (cmds[i + 1] && cmds[i]->type == ';' && cmds[i + 1]->type == ';'
+		&& !cmds[i]->str
+		&& (ft_streql(cmds[i + 1]->str, " ") || !cmds[i + 1]->str))
+	{
+		ft_error("syntax error near unexpected token", ";;");
+		return (false);
+	}
+	else if (cmds[i]->type == ';')
+		cmds[i]->type = 0;
+	return (true);
+}
+
+static bool	is_double_separator(t_cmd **cmds, int i)
+{
+	char	*str_error;
+
+	if (cmds[i + 1] && cmds[i]->type != 0 && cmds[i + 1]->type != 0
+		&& ft_streql(cmds[i]->str, " ")
+		&& (ft_streql(cmds[i + 1]->str, " ") || !cmds[i + 1]->str))
+	{
+		str_error = string_char(cmds[i + 1]->type);
+		ft_error("syntax error near unexpected token", str_error);
+		free(str_error);
+		return (false);
+	}
+	return (true);
+}
+
+bool	cmds_is_valid(t_cmd	**cmds)
+{
+	int		i;
+
+	i = -1;
+	if (!first_is_command(cmds))
+		return (false);
 	while (cmds[++i])
 	{
 		if (cmds[i]->type == 0)
 			continue ;
-		if (cmds[i + 1] && cmds[i]->type == ';' && cmds[i + 1]->type == ';'
-			&& !cmds[i]->str
-			&& (ft_streql(cmds[i + 1]->str, " ") || !cmds[i + 1]->str))
-		{
-			ft_error("syntax error near unexpected token", ";;");
+		if (!is_double_semicolon(cmds, i)
+			|| !is_double_separator(cmds, i))
 			return (false);
-		}
-		else if (cmds[i]->type == ';')
-			cmds[i]->type = 0;
-		if (cmds[i + 1] && cmds[i]->type != 0 && cmds[i + 1]->type != 0
-			&& ft_streql(cmds[i]->str, " ")
-			&& (ft_streql(cmds[i + 1]->str, " ") || !cmds[i + 1]->str))
-		{
-			str_error = string_char(cmds[i + 1]->type);
-			ft_error("syntax error near unexpected token", str_error);
-			free(str_error);
-			return (false);
-		}
 	}
 	return (true);
 }
