008acdee7b7c183efd592352b2f628b787e3ae3c
Ledda Bryan <louchebem06@gmail.com>
2021-07-23 16:02:43 +0200

    2021-07-23T16:02:42+02:00

--- header/inc.h
+++ header/inc.h
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 17:54:58 by bledda            #+#    #+#             */
-/*   Updated: 2021/07/23 16:00:05 by bledda           ###   ########.fr       */
+/*   Updated: 2021/07/23 16:02:32 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 void	ft_pwd(void);
 void	ft_echo(char **argv);
 void	ft_exit(void);
-void	ft_echo(char **argv);
 void	ft_export(char **argv);
 void	ft_env(char **argv);
 void	ft_cd(char **argv);
