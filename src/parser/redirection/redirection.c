0c3a93d355d5e8cbaf07e65651da868e110b1b7b
Bryan Ledda <bledda@student.42nice.fr>
2021-08-04 17:28:14 +0200

    s

--- src/parser/redirection/redirection.c
+++ src/parser/redirection/redirection.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 10:36:47 by mmehran           #+#    #+#             */
-/*   Updated: 2021/08/04 16:26:18 by bledda           ###   ########.fr       */
+/*   Updated: 2021/08/04 17:27:16 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ static bool	ft_create_file(char *str)
 {
 	int	fd;
 
-	fd = open(str, O_CREAT);
+	//argv = parsing(str);
+	fd = open(str, O_CREAT | O_APPEND | O_WRONLY, 0777);;
 	if (fd == -1)
 		return (false);
 	close (fd);
 	return (true);
 }
 
-static void	redirection(t_cmd **cmds, int *i)
+static bool	redirection(t_cmd **cmds, int *i)
 {
 	int	save_i;
 
@@ -41,23 +42,24 @@ static void	redirection(t_cmd **cmds, int *i)
 	}
 	else if (cmds[*i + 1] && cmds[*i + 1]->type == '|')
 		ft_pipe(cmds[save_i], cmds[++(*i)]);
-	else if (cmds[*i + 1]
-		&& (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
+	else if (cmds[*i + 1] && (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
 	{
-		while (cmds[*i + 1]
-			&& (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
+		while (cmds[*i + 1] && (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
 		{
 			(*i)++;
-			if (cmds[*i + 1]
-				&& (cmds[*i + 1]->type == CHEVRON_RR
-					|| cmds[*i + 1]->type == '>'))
-				ft_create_file(cmds[*i]->str);
+			if (cmds[*i + 1] && (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>')
+				&& !ft_create_file(cmds[*i]->args[0]))
+			{
+				ft_error(cmds[*i]->str, "Is a directory", 1);
+				return (false);
+			}
 		}
 		if (cmds[*i]->type == '>')
 			r_chevron(cmds[save_i], cmds[*i]);
 		else if (cmds[*i]->type == CHEVRON_RR)
 			rr_chevron(cmds[save_i], cmds[*i]);
 	}
+	return (true);
 }
 
 void	try_cmds(t_cmd	**cmds)
@@ -69,7 +71,10 @@ void	try_cmds(t_cmd	**cmds)
 	while (cmds[++i])
 	{
 		if (cmds[i + 1] && cmds[i + 1]->type)
-			redirection(cmds, &i);
+		{
+			if (!redirection(cmds, &i))
+				break ;
+		}
 		else if (cmds[i]->str)
 		{
 			argv = cmds[i]->args;
