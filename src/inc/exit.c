27a25efa9337782a483a4b18a12d679daff18d83
mathieu <mmehran@student.42nice.fr>
2021-08-05 12:33:08 +0200

    2021-08-05T12:33:08+02:00

--- src/inc/exit.c
+++ src/inc/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
+/*   By: mmehran <mmehran@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 05:42:10 by bledda            #+#    #+#             */
-/*   Updated: 2021/08/05 12:13:19 by bledda           ###   ########.fr       */
+/*   Updated: 2021/08/05 12:31:57 by mmehran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,16 @@
 
 void	ft_exit(char **av)
 {
+	g_global.return_code = 1;
 	printf("exit\n");
-	if (count_array(av) == 1)
-		g_global.return_code = 1;
-	else if (count_array(av) > 2)
+	if (count_array(av) > 1)
 	{
-		if (ft_strisdigit(av[1]))
+		if (!ft_strisdigit(av[1]))
+			ft_error2("exit", av[1], "numeric argument required", 2);
+		else if (count_array(av) != 2)
 			ft_error2("exit", av[1], "too many arguments", 1);
 		else
-		{
-			ft_error2("exit", av[1], "numeric argument required", 255);
-			return ;
-		}
-	}
-	else if (count_array(av) == 2)
-	{
-		if (ft_strisdigit(av[1]))
 			g_global.return_code = ft_atoi(av[1]);
-		else
-		{
-			ft_error2("exit", av[1], "numeric argument required", 255);
-			return ;
-		}
 	}
 	exit(g_global.return_code);
 }
