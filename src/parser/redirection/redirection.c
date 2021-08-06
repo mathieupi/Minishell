1c7db915c8b5d181bd8fc35af323f80a455e003e
Ledda Bryan <louchebem06@gmail.com>
2021-08-06 17:26:48 +0200

    2021-08-06T17:26:48+02:00

--- src/parser/redirection/redirection.c
+++ src/parser/redirection/redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmehran <mmehran@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 17:28:17 by bledda            #+#    #+#             */
-/*   Updated: 2021/08/06 14:15:57 by mmehran          ###   ########.fr       */
+/*   Updated: 2021/08/06 17:22:44 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,19 +63,105 @@ static bool	red_right(t_cmd **cmds, int *i)
 	return (true);
 }
 
+static bool simple(t_cmd **cmds, int *j)
+{
+	int i = *j;
+
+	if (cmds[i + 1] && cmds[i + 1]->type == '|')
+	{
+		while (cmds[i + 1] && cmds[i + 1]->type == '|')
+			i++;
+		if (cmds[i + 1] && cmds[i + 1]->type != '|')
+			return (false);
+	}
+	else if (cmds[i + 1]
+		&& (cmds[i + 1]->type == CHEVRON_RR || cmds[i + 1]->type == '>'))
+	{
+		while (cmds[i + 1]
+			&& (cmds[i + 1]->type == CHEVRON_RR || cmds[i + 1]->type == '>'))
+			i++;
+		if (cmds[i + 1]
+			&& (cmds[i + 1]->type != CHEVRON_RR || cmds[i + 1]->type != '>'))
+			return (false);
+	}
+	else if (cmds[i + 1]
+		&& (cmds[i + 1]->type == CHEVRON_LL || cmds[i + 1]->type == '<'))
+	{
+		while (cmds[i + 1]
+			&& (cmds[i + 1]->type == CHEVRON_LL || cmds[i + 1]->type == '<'))
+			i++;
+		if (cmds[i + 1]
+			&& (cmds[i + 1]->type != CHEVRON_LL || cmds[i + 1]->type != '<'))
+			return (false);
+	}
+	return (true);
+}
+
 static bool	redirection(t_cmd **cmds, int *i)
 {
-	if (cmds[*i + 1]
-		&& (cmds[*i + 1]->type == CHEVRON_LL || cmds[*i + 1]->type == '<'))
-		red_left(cmds, i);
-	else if (cmds[*i + 1] && cmds[*i + 1]->type == '|')
+	int save_i;
+	int fin;
+	int fout;
+
+	save_i = *i;
+	fin = -1;
+	fout = -1;
+	if (simple(cmds, i))
 	{
-		multi_pipe(cmds, i);
-		return (false);
+		if (cmds[*i + 1] && cmds[*i + 1]->type == '|')
+		{
+			multi_pipe(cmds, i);
+			return (false);
+		}
+		else if (cmds[*i + 1]
+			&& (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
+		{
+			if (red_right(cmds, i))
+				return (false);
+		}
+		else if (cmds[*i + 1]
+			&& (cmds[*i + 1]->type == CHEVRON_LL || cmds[*i + 1]->type == '<'))
+			red_left(cmds, i);
 	}
-	else if (cmds[*i + 1]
-		&& (cmds[*i + 1]->type == CHEVRON_RR || cmds[*i + 1]->type == '>'))
-		red_right(cmds, i);
+	else if (!simple(cmds, i))
+	{
+		while (cmds[save_i + 1] && cmds[save_i + 1]->type == '|')
+			save_i++;
+		while (cmds[save_i + 1]
+			&& (cmds[save_i + 1]->type == CHEVRON_RR || cmds[save_i + 1]->type == '>'))
+			save_i++;
+		if ((cmds[save_i]->type == CHEVRON_RR || cmds[save_i]->type == '>'))
+			fout = save_i;
+		while (cmds[save_i + 1]
+			&& (cmds[save_i + 1]->type == CHEVRON_LL || cmds[save_i + 1]->type == '<'))
+		{
+			if (!ft_create_file(cmds[*i]->args[0]))
+			{
+				ft_error(cmds[*i]->str, "Is a directory", 1);
+				return (false);
+			}
+			save_i++;
+		}
+		if ((cmds[save_i]->type == CHEVRON_LL || cmds[save_i]->type == '<'))
+			fin = save_i;
+		if (fout == -1)
+		{
+			while (cmds[save_i + 1]
+				&& (cmds[save_i + 1]->type == CHEVRON_RR || cmds[save_i + 1]->type == '>'))
+			{
+				if (!ft_create_file(cmds[*i]->args[0]))
+				{
+					ft_error(cmds[*i]->str, "Is a directory", 1);
+					return (false);
+				}
+				save_i++;
+			}
+			if ((cmds[save_i]->type == CHEVRON_RR || cmds[save_i]->type == '>'))
+				fout = save_i;
+		}
+		printf("%s\n%s\n%s\n",cmds[*i]->str, cmds[fin]->str, cmds[fout]->str);
+		pause();
+	}
 	return (true);
 }
 
