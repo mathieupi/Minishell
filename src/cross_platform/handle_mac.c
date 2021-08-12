ab9102df5c8c023aa7479e65fdeda3b45e3c9ef7
Bryan Ledda <bledda@student.42nice.fr>
2021-08-12 22:34:35 +0200

    Thu Aug 12 22:34:35 CEST 2021

--- src/cross_platform/handle_mac.c
+++ src/cross_platform/handle_mac.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 19:40:21 by bledda            #+#    #+#             */
-/*   Updated: 2021/07/15 19:53:03 by bledda           ###   ########.fr       */
+/*   Updated: 2021/08/12 22:01:07 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 void	handle(int sig)
 {
+	printf("\r");
+	rl_on_new_line();
+	rl_redisplay();
 	if (sig == SIGINT)
 	{
-		rl_on_new_line();
-		rl_redisplay();
 		printf("  \b\b\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
 	else if (sig == SIGQUIT)
-	{
-		rl_on_new_line();
-		rl_redisplay();
 		printf("  \b\b");
-	}
 }
